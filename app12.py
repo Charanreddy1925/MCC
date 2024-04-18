2a9fd7dcec4583acdb2f1ae8d9d8bb00975e86cb
Charanreddy1925 <158473985+Charanreddy1925@users.noreply.github.com>
2024-04-18 12:16:31 +0530

    Update app12.py

--- app12.py
+++ app12.py
@@ -19,7 +19,7 @@ def predict_cluster(Education, Marital_Status, Income, Kids, Expenses,
     # Predict the cluster label using the trained model
                         
     features = [Education, Marital_Status, Income, Kids, Expenses,
-                                  TotalAcceptedCmp, NumTotalPurchases, Customer_Age, Customer_For] 
+                                  TotalAcceptedCmp, NumTotalPurchases, Customer_Age] +0*18
     prediction = model.predict([features])
     return prediction
 
